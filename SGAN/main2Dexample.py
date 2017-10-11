db3a6163f889da11c0901ca672ef85b95e23ba3e
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-10-11 16:58:59 +0000

    samples path need to end with a "/"

--- SGAN/main2Dexample.py
+++ SGAN/main2Dexample.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         sgancfg="demo.sgancfg",
         data_provider=get_texture_iter("ti_2D/"),
         run_name="Demo",
-        checkpoints_dir="demo/checkpoints",
-        logs_dir="demo/logs",
-        samples_dir="demo/samples",
+        checkpoints_dir="demo/checkpoints/",
+        logs_dir="demo/logs/",
+        samples_dir="demo/samples/",
         use_tensorboard=True,
         plot_models=True)
