4aaacd5a9d413c83a883c986fce98908ed37b93a
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-10-11 15:31:49 +0000

    Renaming main to example

--- SGAN/main2Dexample.py
+++ SGAN/main2Dexample.py
@@ -1,9 +1,10 @@
 from training import train
+from data_io2D import get_texture_iter
 
 if __name__ == "__main__":
     train(
         sgancfg="demo.sgancgf",
-        training_image="ti_2D/ti_2500_1ch",
+        data_provider=get_texture_iter("ti_2D"),
         run_name="Demo",
         checkpoints_dir="demo/checkpoints",
         logs_dir="demo/logs",
