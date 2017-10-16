73e37bef1726e66139631e6f9d1345c4aa4a4c99
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-10-16 15:55:12 +0000

    Minor changes in the main

--- SGAN/main2Dexample.py
+++ SGAN/main2Dexample.py
@@ -1,14 +1,13 @@
 from training import train
 from data_io2D import get_texture_iter
-from config import Config
 
 if __name__ == "__main__":
     train(
         sgancfg="demo.sgancfg",
-        data_provider=get_texture_iter("ti_2D/"),
+        data_provider=get_texture_iter("ti_2D/", batch_size=24),
         run_name="Demo",
         checkpoints_dir="demo/checkpoints/",
         logs_dir="demo/logs/",
         samples_dir="demo/samples/",
         use_tensorboard=True,
-        plot_models=True)
+        plot_models=False)
