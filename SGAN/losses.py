ceff51d4cb126381c86f926dfca44023bd04b07a
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-10-18 10:32:55 +0000

    Adding epsilon to the logs in the losses : fixes NaN and Inf losses

--- SGAN/losses.py
+++ SGAN/losses.py
@@ -2,8 +2,8 @@ import keras.backend as K
 
 
 def loss_true(y_true, y_pred):
-    return -K.mean(K.log(K.flatten(y_pred)))
+    return -K.mean(K.log(K.flatten(y_pred) + K.epsilon()))
 
 
 def loss_fake(y_true, y_pred):
-    return -K.mean(K.log(1 - K.flatten(y_pred)))
+    return -K.mean(K.log(1 - K.flatten(y_pred) + K.epsilon()))
