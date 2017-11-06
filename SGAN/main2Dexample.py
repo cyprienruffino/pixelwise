1e328e7070a7423694f41e8bb956ecdefa200ca2
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-11-06 15:32:42 +0100

    Updating example

--- SGAN/main2Dexample.py
+++ SGAN/main2Dexample.py
@@ -1,14 +1,17 @@
 from training import train
 from data_io2D import get_texture_iter
 from config import Config
+from datetime import datetime
 
 if __name__ == "__main__":
+    run_name = str(datetime.now())
+    config = Config(run_name)
     train(
-        sgancfg="demo.sgancfg",
+        sgancfg=config,
         data_provider=get_texture_iter("ti_2D/", batch_size=24),
-        run_name="Demo",
-        checkpoints_dir="demo/checkpoints/",
-        logs_dir="demo/logs/",
-        samples_dir="demo/samples/",
+        run_name=run_name,
+        checkpoints_dir="runs/" + run_name + "/checkpoints/",
+        logs_dir="runs/" + run_name + "/logs/",
+        samples_dir="runs/" + run_name + "/samples/",
         use_tensorboard=True,
         plot_models=False)
