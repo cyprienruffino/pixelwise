8a52a85ff92bee7bf99ccf6e74aea98bb2dec525
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-11-06 15:27:53 +0100

    Freezing each model layer by layer

--- SGAN/sgan.py
+++ SGAN/sgan.py
@@ -182,20 +182,24 @@ def sgan(config):
     D = Model(inputs=X, outputs=D_out, name="D")
     G = Model(inputs=Z, outputs=G_out, name="G")
 
-    G.trainable = False
+    for layer in G.layers:
+        layer.trainable = False
     Adv = Model(inputs=[X, Z], outputs=[D(X), D(G(Z))], name="Adv")
     # Keras sums the losses
     Adv.compile(
         optimizer=Adam(lr=config.lr, beta_1=config.b1),
         loss=[loss_true, loss_fake],
         loss_weights=[1, 1])
-    G.trainable = True
     TimePrint("Discriminator done.")
 
-    D.trainable = False
+    for layer in D.layers:
+        layer.trainable = False
+    for layer in G.layers:
+        layer.trainable = True
     DG = Model(inputs=Z, outputs=D(G(Z)), name="DG")
     DG.compile(optimizer=Adam(lr=config.lr, beta_1=config.b1), loss=loss_gen)
-    D.trainable = True
     TimePrint("Generator done.")
+    for layer in D.layers:
+        layer.trainable = True
 
     return D, G, DG, Adv
