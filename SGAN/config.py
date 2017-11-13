c4bcd0abb062f678a5015b6cfefad43d2a94ff8a
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-11-13 16:29:29 +0100

    Minor changes in config Adding support for other optimizers

--- SGAN/config.py
+++ SGAN/config.py
@@ -27,10 +27,31 @@ class GenUpscaling(Enum):
     upsampling = "upsampling"
 
 
+class Optimizer(Enum):
+    adam = "adam"
+    rmsprop = "rmsprop"
+    sgd = "sgd"
+
+
 class Config:
     def __init__(self, name):
 
         self.name = name
+
+        # Hyperparameters
+        self.optimizer = Optimizer.adam
+        self.lr = 0.0005  # learning rate
+        self.b1 = 0.5  # Adam momentum term
+        self.momentum = 0.9  # SGD momentum term
+        self.l2_fac = 1e-5  # L2 weight regularization factor
+        self.batch_size = 32
+        self.epoch_iters = self.batch_size * 100
+        self.epochs = 40
+        self.k = 1  # Number of D updates vs G updates
+
+        # Seed, for reproductibility. Set np.random.seed with this
+        self.seed = int(hashlib.sha1(name.encode("utf-8")).hexdigest(), 16) % (
+            10**8)
 
         # Network setup
         # GAN or Wasserstein GAN
@@ -40,12 +61,12 @@ class Config:
         self.noise = True
         self.c = 0.01
 
+        # Upsampling method
         self.gen_up = GenUpscaling.deconvolution
 
         # Depth
         self.gen_depth = 5
         self.dis_depth = 5
-
 
         # Data dimensions
         self.convdims = 2  # 2D or 3D convolutions
@@ -61,27 +82,13 @@ class Config:
         self.nc = 1  # Number of channels
 
         # Number of filters
-        self.gen_fn = [2**(n + 6) for n in range(self.gen_depth - 1, 0, -1)] + [self.nc]
+        self.gen_fn = [2**(n + 6)
+                       for n in range(self.gen_depth - 1, 0, -1)] + [self.nc]
         self.dis_fn = [2**(n + 6) for n in range(self.dis_depth - 1)] + [1]
- 
+
         # Strides
         self.gen_strides = [2] * self.gen_depth
         self.dis_strides = [2] * self.dis_depth
-
-        # Hyperparameters
-        self.lr = 0.0005  # learning rate of adam
-        self.b1 = 0.5  # momentum term of adam
-        self.l2_fac = 1e-5  # L2 weight regularization factor
-
-        # Seed, for reproductibility. Set np.random.seed with this
-        self.seed = int(hashlib.sha1(name.encode("utf-8")).hexdigest(), 16) % (
-            10**8)
-
-        # Learning parameters
-        self.batch_size = 32
-        self.epoch_iters = self.batch_size * 100
-        self.epochs = 30
-        self.k = 1  # Number of D updates vs G updates
 
 
 if __name__ == "__main__":
