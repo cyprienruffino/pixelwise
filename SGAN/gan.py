08a12c0e1b3bb52bff5b4dd99945a89758fa4d94
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-12-04 11:31:46 +0100

    Fixing typo : missing * when calling the optimizer with the custom parameters

--- SGAN/gan.py
+++ SGAN/gan.py
@@ -8,7 +8,7 @@ def gan(config):
 
     # Setting up
     loss_true, loss_fake, loss_gen = get_losses(config.losses)
-    optimizer = get_optimizer(config.optimizer)(*config.optimizer_params)
+    optimizer = get_optimizer(config.optimizer)(**config.optimizer_params)
     G = get_generator(config.generator)
     D = get_discriminator(config.discriminator)
 
