52a6e86e04abe155dbaf7ce18c87c4dd2b7e2805
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2017-12-04 11:28:00 +0100

    New config.py template, with respect to the new factory module

--- SGAN/config.py
+++ SGAN/config.py
@@ -3,42 +3,42 @@ import pickle
 import sys
 import datetime
 
+from factory import discriminators, generators, losses, optimizers
+
 
 class Config:
     def __init__(self, name):
+
+        # Run metadata
         self.name = name
-
-        # Hyperparameters
-        self.optimizer = Optimizer.adam
-        self.lr = 0.0005  # learning rate
-        self.b1 = 0.5  # Adam momentum term
-        self.momentum = 0.9  # SGD momentum term
-        self.batch_size = 24
-        self.epoch_iters = self.batch_size * 100
-        self.epochs = 40
-        self.k = 1  # Number of D updates vs G updates
-
-        self.optimizer_params = {"lr": self.lr, "beta_1": self.b1}
-
-        # Seed, for reproductibility. Set np.random.seed with this
         self.seed = int(hashlib.sha1(name.encode("utf-8")).hexdigest(), 16) % (
             10**8)
 
-        # Network setup
-        # GAN or Wasserstein GAN
-
-        self.losses = Losses.wasserstein_gan
-        self.clip_weights = False
-        self.clip_gradients = False
-        self.c = 0.01
+        # Training settings
+        self.batch_size = 24
+        self.epoch_iters = self.batch_size * 100
+        self.epochs = 40
+        self.k = 5  # Number of D updates vs G updates
 
         # Data dimensions
         self.convdims = 2  # 2D or 3D convolutions
         self.nz = 1  # num of dim for Z at each field position (d in the paper)
         self.zx = 12  # num of spatial dimensions in Z (l and m in the paper)
         self.zx_sample = 12  # size of the spatial dimension in Z
-        # num of pixels width/height of images in X
         self.nc = 1  # Number of channels
+
+        # Network setup
+        self.discriminator = discriminators.classical_sgan
+        self.generator = generators.classical_sgan
+        self.losses = losses.wasserstein_gan
+        self.clip_weights = True  # Clip the discriminator weights (cf Wasserstein GAN)
+        self.c = 0.01  # Clipping value
+
+        # Optimizer
+        self.optimizer = optimizers.adam
+        self.lr = 0.0005  # learning rate
+        self.b1 = 0.5  # Adam momentum term
+        self.optimizer_params = {"lr": self.lr, "beta_1": self.b1}
 
 
 if __name__ == "__main__":
