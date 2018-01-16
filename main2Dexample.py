709f05a18c75ceaaee4e3708dbbe020d0300c537
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2018-01-16 12:11:09 +0100

    Improvements in the main2D file

--- main2Dexample.py
+++ main2Dexample.py
@@ -1,21 +1,26 @@
 import os
+import datetime
 
+from config import Config
 from training import train
 from data_io2D import get_texture_iter
-from config import Config
-from datetime import datetime
 
-if __name__ == "__main__":
-    run_name = str(datetime.now())
-    config = Config(run_name)
+
+def run():
+    run_name = str(datetime.datetime.now())
     os.mkdir("runs/" + run_name)
     print(run_name)
+    config = Config(run_name)
     train(
         sgancfg=config,
-        data_provider=get_texture_iter("ti_2D/", batch_size=config.batch_size),
+        data_provider=get_texture_iter("ti_2D/", batch_size=config.batch_size, npx=config.npx, n_channel=config.nc, mirror=False),
         run_name=run_name,
         checkpoints_dir="runs/" + run_name + "/checkpoints/",
         logs_dir="runs/" + run_name + "/logs/",
         samples_dir="runs/" + run_name + "/samples/",
         use_tensorboard=True,
         plot_models=False)
+
+
+if __name__ == "__main__":
+    run()
