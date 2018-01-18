1a70ae86aa4aa78629f7491b220fdd402170024c
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2018-01-18 17:23:06 +0100

    First commit on the branch

--- my_discriminators.py
+++ my_discriminators.py
@@ -1,12 +1,11 @@
-def classical_sgan_disc(convdims=2,
+
+
+def classical_sgan_disc(npx,
+                        convdims=2,
                         channels=1,
                         clip_weights=False,
                         clipping_value=0.01):
     from kgan.constraints import Clip
-<<<<<<< HEAD
-    from kgan.layers import LayerNormalization
-=======
->>>>>>> 0b5c0d0697be2f2b4d201a251999cd38520fe751
     from keras.engine import Model
     from keras.layers import (BatchNormalization, Conv2D, Conv3D, Input,
                               LeakyReLU, GaussianNoise)
@@ -16,11 +15,7 @@ def classical_sgan_disc(convdims=2,
     conv_kernel = 9
     l2_fac = 1e-5
     strides = 2
-<<<<<<< HEAD
     epsilon = 1e-4
-=======
-    epsilon=1e-4
->>>>>>> 0b5c0d0697be2f2b4d201a251999cd38520fe751
     convs = [64, 128, 256, 512, 1]
     init = RandomNormal(stddev=0.02)
 
@@ -35,7 +30,7 @@ def classical_sgan_disc(convdims=2,
     else:
         W_constraint = None
 
-    X = Input((channels, ) + (None, ) * convdims, name="X")
+    X = Input((channels, ) + (npx, ) * convdims, name="X")
 
     # Discriminator
     layer = GaussianNoise(stddev=0.1)(X)
@@ -63,10 +58,6 @@ def classical_sgan_disc(convdims=2,
             data_format="channels_first",
             kernel_constraint=W_constraint)(layer)
         layer = LeakyReLU()(conv)
-<<<<<<< HEAD
-=======
-        layer = BatchNormalization(axis=1, epsilon=epsilon)(layer)
->>>>>>> 0b5c0d0697be2f2b4d201a251999cd38520fe751
 
     D_out = Conv(
         filters=convs[-1],
@@ -77,11 +68,6 @@ def classical_sgan_disc(convdims=2,
         kernel_initializer=init,
         kernel_regularizer=l2(l2_fac),
         data_format="channels_first",
-<<<<<<< HEAD
         kernel_constraint=W_constraint)(layer)
-=======
-        kernel_constraint=W_constraint,
-        name="D_out")(layer)
->>>>>>> 0b5c0d0697be2f2b4d201a251999cd38520fe751
 
     return Model(inputs=X, outputs=D_out, name="D")
--- my_generators.py
+++ my_generators.py
@@ -1,4 +1,7 @@
-def classical_sgan_gen(convdims=2,
+
+
+def classical_sgan_gen(zx,
+                       convdims=2,
                        channels=1,
                        clip_weights=False,
                        clipping_value=0.01):
@@ -16,17 +19,13 @@ def classical_sgan_gen(convdims=2,
     init = RandomNormal(stddev=0.02)
     conv = None
     G_out = None
-<<<<<<< HEAD
     deconv_kernel = 5
-=======
-    deconv_kernel = 9
->>>>>>> 0b5c0d0697be2f2b4d201a251999cd38520fe751
     l2_fac = 1e-5
     epsilon = 1e-4
     deconvs = [512, 256, 128, 64, 1]
 
     # Generator
-    Z = Input((channels, ) + (None, ) * convdims, name="Z")
+    Z = Input((channels, ) + (zx, ) * convdims, name="Z")
     layer = Z
 
     for l in range(len(deconvs) - 1):
@@ -49,11 +48,6 @@ def classical_sgan_gen(convdims=2,
         activation="tanh",
         kernel_initializer=init,
         kernel_regularizer=l2(l2_fac),
-<<<<<<< HEAD
         data_format="channels_first")(layer)
-=======
-        data_format="channels_first",
-        name="G_out")(layer)
->>>>>>> 0b5c0d0697be2f2b4d201a251999cd38520fe751
 
     return Model(inputs=Z, outputs=G_out, name="G")
