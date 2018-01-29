4457851a75cc7510f66c04269ef19010e4b071a8
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2018-01-29 16:45:19 +0100

    Various fixes

--- src/main2D.py
+++ src/main2D.py
@@ -8,16 +8,16 @@ from data_io2D import get_texture_iter
 
 def run():
     run_name = str(datetime.datetime.now())
-    os.mkdir("../runs/" + run_name)
+    os.mkdir("./runs/" + run_name)
     print(run_name)
     config = Config(run_name)
     train(
         sgancfg=config,
-        data_provider=get_texture_iter("../ti_2D/", batch_size=config.batch_size, npx=config.npx, n_channel=config.nc, mirror=False),
+        data_provider=get_texture_iter("./ti_2D/", batch_size=config.batch_size, npx=config.npx, n_channel=config.nc, mirror=False),
         run_name=run_name,
-        checkpoints_dir="../runs/" + run_name + "/checkpoints/",
-        logs_dir="../runs/" + run_name + "/logs/",
-        samples_dir="../runs/" + run_name + "/samples/",
+        checkpoints_dir="./runs/" + run_name + "/checkpoints/",
+        logs_dir="./runs/" + run_name + "/logs/",
+        samples_dir="./runs/" + run_name + "/samples/",
         use_tensorboard=True,
         plot_models=False)
 
