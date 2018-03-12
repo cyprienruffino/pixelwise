e01c215fc48b63e8ed48b5626dfe92da0d742d6b
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2018-03-12 09:26:16 +0100

    Adding list-typed input in gan(), for multiple input gan (cgan)

--- src/gan.py
+++ src/gan.py
@@ -4,10 +4,15 @@ def gan(discriminator, generator, loss_disc_true, loss_disc_fake, loss_gen, opti
     X = discriminator.input
     Z = generator.input
 
+    if type(X) is not list:
+        X = [X]
+    if type(Z) is not list:
+        Z = [Z]
+
     for layer in generator.layers:
         layer.trainable = False
 
-    Adv = Model(inputs=[X, Z], outputs=[discriminator(X), discriminator(generator(Z))], name="Adv")
+    Adv = Model(inputs=X + Z, outputs=[discriminator(X), discriminator(generator(Z))], name="Adv")
     Adv.compile(optimizer=optimizer, loss=[loss_disc_true, loss_disc_fake])
 
     for layer in discriminator.layers:
